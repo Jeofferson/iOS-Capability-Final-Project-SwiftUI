38e4fa70385053ec00f996c8ea7aabe3f4500c6f
Jeofferson Dela Peña <jdelapena@stratpoint.com>
2022-10-05 14:35:50 +0800

    Initial Commit

---
+++ iOS Capability Final Project (SwiftUI)/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS Capability Final Project (SwiftUI)
+//
+//  Created by Jeofferson Dela Pena on 10/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Capability Final Project (SwiftUI)/iOS_Capability_Final_Project__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Capability_Final_Project__SwiftUI_App.swift
+//  iOS Capability Final Project (SwiftUI)
+//
+//  Created by Jeofferson Dela Pena on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Capability_Final_Project__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
